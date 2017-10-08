977c49dacca3f7284902c86812159226d10b88e2
Salah Kaci <salah.kaci@gmail.com>
2017-10-08 15:48:18 +0200

    [Search] Rename view controller

---
+++ MyPixabay/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  MyPixabay
+//
+//  Created by Salah Kaci on 08/10/2017.
+//  Copyright © 2017 Salah Kaci. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
