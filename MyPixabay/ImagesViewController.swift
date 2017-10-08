933181d291e40c040d3eb49446accbeaf34d1897
Salah Kaci <salah.kaci@gmail.com>
2017-10-08 19:44:19 +0200

    [Images] Fix images constraints in scrollview

--- MyPixabay/ImagesViewController.swift
+++ MyPixabay/ImagesViewController.swift
@@ -12,7 +12,6 @@ import UIKit
 class ImagesViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var pageControl: UIPageControl!
     
     var images = [Image]()
@@ -26,38 +25,40 @@ class ImagesViewController: UIViewController, UIScrollViewDelegate {
     }
     
     func setScrollViewContent() {
+        self.scrollView.backgroundColor = UIColor.black
         self.scrollView.translatesAutoresizingMaskIntoConstraints = false
         
-        var previousImageView: UIImageView?
         for image in self.images {
-            let imageView = UIImageView()
-            imageView.loadImageFrom(url: image.url)
-            imageView.contentMode = .scaleAspectFill
-            previousImageView = imageView
-            contentView.addSubview(imageView)
-            
-            var constraints = [NSLayoutConstraint]()
-            constraints.append(NSLayoutConstraint(item: imageView, attribute: .width, relatedBy: .equal, toItem: self.scrollView, attribute: .width, multiplier: 1, constant: 0))
-            constraints.append(NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal, toItem: self.scrollView, attribute: .height, multiplier: 1, constant: 0))
-            
-            let index = self.images.index(where: { $0.id == image.id })
-            if let index = index, index == 0 {
-                constraints.append(NSLayoutConstraint(item: imageView, attribute: .leading, relatedBy: .equal, toItem: self.scrollView, attribute: .leading, multiplier: 1, constant: 0))
-            }
-            if let index = index, index == (images.count - 1) {
-                constraints.append(NSLayoutConstraint(item: imageView, attribute: .trailing, relatedBy: .equal, toItem: self.scrollView, attribute: .trailing, multiplier: 1, constant: 0))
-            }
-            if let previousImageView = previousImageView {
-                constraints.append(NSLayoutConstraint(item: previousImageView, attribute: .trailing, relatedBy: .equal, toItem: imageView, attribute: .leading, multiplier: 1, constant: 0))
-            }
-            self.scrollView.addConstraints(constraints)
+            addImageView(fromImage: image)
         }
         
         self.scrollView.contentSize = CGSize(width:self.scrollView.frame.size.width * CGFloat(self.images.count), height: self.scrollView.frame.size.height)
         pageControl.addTarget(self, action: #selector(self.changePage), for: UIControlEvents.valueChanged)
     }
     
-    func configurePageControl() {
+    private func addImageView(fromImage image: Image) {
+        let previousImageView = self.scrollView.subviews.last
+        let imageView = UIImageView()
+        imageView.loadImageFrom(url: image.url)
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = UIColor.black
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        self.scrollView.addSubview(imageView)
+        
+        let index = self.images.index(where: { $0.id == image.id })
+        let isFirst = index != nil && index! == 0
+        let isLast = index != nil && index! == (self.images.count - 1)
+        
+        let vFormat = "V:|[image]|"
+        var hFormat = "H:\(isFirst ? "|" : "")"
+        hFormat = "\(hFormat)\(previousImageView != nil ? "[prevImage]" : "")"
+        hFormat = "\(hFormat)[image(==scrollView)]"
+        hFormat = "\(hFormat)\(isLast ? "|" : "")"
+        self.scrollView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: vFormat, options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["image":imageView]))
+        self.scrollView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: hFormat, options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["image":imageView, "prevImage": previousImageView, "scrollView": scrollView]))
+    }
+    
+    private func configurePageControl() {
         self.pageControl.numberOfPages = images.count
         self.pageControl.currentPage = 0
         self.pageControl.currentPageIndicatorTintColor = UIColor.blue
