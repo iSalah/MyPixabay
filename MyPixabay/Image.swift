04dff344e30accd3b8bd1e10e608d3555a2487f5
Salah Kaci <salah.kaci@gmail.com>
2017-10-08 18:03:37 +0200

    [Search] Allow multi-selection

--- MyPixabay/Image.swift
+++ MyPixabay/Image.swift
@@ -9,13 +9,16 @@
 import Foundation
 
 class Image {
+    var id: Int
     var url: String
     var previewUrl: String
     
     init?(fromDictionary dict: [String:Any]) {
         guard let url = dict["webformatURL"] as? String else { return nil }
         guard let previewUrl = dict["previewURL"] as? String else { return nil }
+        guard let id = dict["id"] as? Int else { return nil }
         self.url = url
         self.previewUrl = previewUrl
+        self.id = id
     }
 }
