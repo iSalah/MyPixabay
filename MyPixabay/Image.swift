ec53186237cf9f4e8fd7389ea78bf9b7ec2373e3
Salah Kaci <salah.kaci@gmail.com>
2017-10-08 15:58:41 +0200

    Add Image model

---
+++ MyPixabay/Image.swift
@@ -0,0 +1,17 @@
+//
+//  Image.swift
+//  MyPixabay
+//
+//  Created by Salah Kaci on 08/10/2017.
+//  Copyright © 2017 Salah Kaci. All rights reserved.
+//
+
+import Foundation
+
+class Image {
+    var url: String
+    
+    init?() {
+        return nil
+    }
+}
