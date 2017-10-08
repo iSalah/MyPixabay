74e00f7a9f751aa5e714eb19fe63cad1e8ddd051
Salah Kaci <salah.kaci@gmail.com>
2017-10-08 16:20:34 +0200

    Add Image service

---
+++ MyPixabay/AppConfig.swift
@@ -0,0 +1,13 @@
+//
+//  AppConfig.swift
+//  MyPixabay
+//
+//  Created by Salah Kaci on 08/10/2017.
+//  Copyright © 2017 Salah Kaci. All rights reserved.
+//
+
+import Foundation
+
+struct AppConfig {
+    static let PixabayAPIKey = "6658770-7e385989c760e537ef301188b"
+}
