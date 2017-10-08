9dc41879375346513dcb36c94fe43a3563f99e45
Salah Kaci <salah.kaci@gmail.com>
2017-10-08 17:24:55 +0200

    Use cache data on requests

--- MyPixabay/RequestsManager.swift
+++ MyPixabay/RequestsManager.swift
@@ -20,7 +20,9 @@ import Foundation
 class RequestManager {
     
     class func get(url: String, withCompletion completion: @escaping (Data?) -> Void) {
-        let session = URLSession(configuration: .ephemeral, delegate: nil, delegateQueue: OperationQueue.main)
+        let config = URLSessionConfiguration.default
+        config.requestCachePolicy = .returnCacheDataElseLoad
+        let session = URLSession(configuration: config, delegate: nil, delegateQueue: OperationQueue.main)
         let url = URL(string: url)!
         session.dataTask(with: url, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
             guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else { return completion(nil) }
