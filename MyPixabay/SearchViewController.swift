b82f5a14682cd006d4487b7b03c5b4c22f3f6c1a
Salah Kaci <salah.kaci@gmail.com>
2017-10-08 15:55:50 +0200

    [Search] Add layout on storyboard

--- MyPixabay/SearchViewController.swift
+++ MyPixabay/SearchViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
